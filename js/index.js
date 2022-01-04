76f9643280b64590d8407b5194f15ee11877d7d3
= <=>
2022-01-04 17:56:06 -0500

    subir proyecto terminado a github

--- js/index.js
+++ js/index.js
@@ -1,27 +1,35 @@
-// Aquí realizamos un la consulta de la promesa, esperando su respuesta asíncrona
-fetch('https://randomuser.me/api/')
+const contenedorPersona = document.querySelector('.tarjeta')
+const boton = document.getElementById('random')
+
+boton.addEventListener('click',() => {
+
+    fetch('https://randomuser.me/api/')
     .then(response => {
         return response.json()
     })
     .then(data => {
         //manipulamos la respuesta
         console.log(data)
+        renderizarDatosUsuario(data.results[0].picture.medium,data.results[0].name.first,data.results[0].email)
     });
 
-function renderizarDatosUsuario(datos) {
-    /* -------------------------------- CONSIGNA 1 -------------------------------- */
-    // Aquí deben desarrollar una función que muestre en pantalla:
-    // la foto, el nombre completo del usuario y su email.
-    // Esto debe estar basado en la info que nos llega desde la API e insertarse en el HTML.
+})
+
+
+function renderizarDatosUsuario(img,nombre,email) {
+    const datosPersona = `
+        <div class="personas">
+            <div>
+                <img src="${img}">
+            </div>
+            <div>
+            <p>${nombre}</p>
+            </div>
+            <div>
+            <p>${email}</p>
+            </div>
+        </div>
+        `
     
-    
-    
-    
-    
+    contenedorPersona.innerHTML = datosPersona    
 }
-
-
-/* --------------------------- CONSIGNA 2 (extra) --------------------------- */
-// Aqui pueden ir por el punto extra de utilizar el boton que se encuentra comentado en el HTML
-// Pueden descomentar el código del index.html y usar ese boton para ejecutar un nuevo pedido a la API, sin necesidad de recargar la página.
-// Es criterio del equipo QUÉ bloque del código debe contenerse dentro de una función para poder ser ejecutada cada vez que se escuche un click.
